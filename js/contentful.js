5330475589b3d1f8ad3c38c8c590fcb562bba9f4
unknown <trunghieu20032502@gmail.com>
2024-06-02 16:32:55 +0700

    first-commit

--- js/contentful.js
+++ js/contentful.js
@@ -9,11 +9,14 @@ client.getEntries().then(function (entries) {
       var div = document.createElement("div");
       div.className = "col-3"
       div.innerHTML = `<div class="card card-custom">
-                         <h4 class="fs-2 fw-4">${entry.fields.internalName}</h4>
-                         <p>${entry.fields.pageTitle ? entry.fields.pageTitle : "Title"}</p>
-                         <p>${entry.fields.pageDescription ? entry.fields.pageDescription : "Description"}</p>
+                         <div class="card-body">
+                          <h4 class="fs-2 fw-4">${entry.fields.internalName}</h4>
+                          <p>${entry.fields.pageTitle ? entry.fields.pageTitle : "Title"}</p>
+                          <p>${entry.fields.pageDescription ? entry.fields.pageDescription : "This is the descriptive paragraph used in this project and it shows the details of this element"}</p>
+                         </div>
                        </div>`;
       container.appendChild(div);
+      console.log(entries)
     });
   })
   .catch(function (error) {
